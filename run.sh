365014e2b435fca5a05577cddf971acfb9cb84db
Dan Leehr <dan.leehr@nescent.org>
2014-04-18 10:35:33 -0400

    Now installs db and runs But OpenID fails. Perhaps ruby too old

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Start the Postgres container
-docker run -d --name postgres peer60/postgres
+# Build a Postgres container
+docker build -t dleehr/traitdb_postgres database
+docker run -d --name traitdb_postgres dleehr/traitdb_postgres
 
 # Build TraitDB
-
-docker build -t dleehr/TraitDB app
-docker run -i --name traitdb dleehr/TraitDB
+docker build -t dleehr/traitdb app
+docker run -P -d --name traitdb --link traitdb_postgres:db dleehr/traitdb
